b7993f29f1de4106ff02a189886e1efc8599d383
JoshuaMumo <jmumo6672@gmail.com>
2026-05-12 01:45:55 +0300

    string

--- string_1/src/main.rs
+++ string_1/src/main.rs
@@ -1,3 +1,38 @@
+struct Person {
+    age: u32,
+    name: String,
+    favorite_color: String,
+}
+
+fn person_info(name: &str, favorite_color: &str) {
+    println!("Name: {}", name);
+    println!("Favorite color: {}", favorite_color);
+    println!("   ");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // people created here so the vector can see them
+    let people = vec![
+        Person {
+            name: String::from("Itachi"),
+            age: 8,
+            favorite_color: String::from("orange"),
+        },
+        Person {
+            name: String::from("Sasuke"),
+            age: 15,
+            favorite_color: String::from("purple"),
+        },
+        Person {
+            name: String::from("Shisui"),
+            age: 10,
+            favorite_color: String::from("green"),
+        },
+    ];
+
+    for person in &people {
+        if person.age <= 10 {
+            person_info(&person.name, &person.favorite_color); // name matches now
+        }
+    }
 }
